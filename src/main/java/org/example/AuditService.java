5e243cea8a989feb82c22122152f1ab392425125
CristinaMaria0 <101529013+CristinaMaria0@users.noreply.github.com>
2023-05-23 19:11:25 +0300

    audit

---
+++ src/main/java/org/example/AuditService.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class AuditService {
+    private static final String FILE_PATH = "audit.csv";
+
+    public static synchronized void logAction(String actionName) {
+        String timestamp = getCurrentTimestamp();
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter(FILE_PATH, true))) {
+            String logEntry = actionName + "," + timestamp;
+            writer.println(logEntry);
+        } catch (Exception e) {
+
+        }
+    }
+
+    private static String getCurrentTimestamp() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date currentDate = new Date();
+        return dateFormat.format(currentDate);
+    }
+}
+
